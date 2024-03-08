ab0c61346cc326a0408bf98ff66bdc6e67c08491
t0rch13 <batyrhan22.11@gmail.com>
2024-03-09 00:59:09 +0500

    feat: update, delete for pets and posts

--- front/src/js/getpet.js
+++ front/src/js/getpet.js
@@ -20,4 +20,75 @@ $(document).ready(function () {
             console.log(error);
         }
     });
+
+    $('#editPetModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) 
+        var petName = $('#pet-name').text() 
+        var petSpecies = $('#pet-species').text()
+        var petBreed = $('#pet-breed').text()
+        var petAge = $('#pet-age').text()
+      
+        // Update the modal's content.
+        var modal = $(this)
+        modal.find('#pet-name-input').val(petName)
+        modal.find('#pet-species-input').val(petSpecies)
+        modal.find('#pet-breed-input').val(petBreed)
+        modal.find('#pet-age-input').val(petAge)
+    })
+      
+      
+    $('#saveChangesButton').click(function() {
+        var petName = $('#pet-name-input').val()
+        var petSpecies = $('#pet-species-input').val()
+        var petBreed = $('#pet-breed-input').val()
+        var petAge = parseInt($('#pet-age-input').val()) // Parse age as integer
+    
+        var data = {
+            petname: petName,
+            species: petSpecies,
+            breed: petBreed,
+            age: petAge
+        };
+    
+        $.ajax({
+            url: `http://localhost:8000/api/pets/${petId}`,
+            method: 'PUT',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('authToken')
+            },
+            data: JSON.stringify(data), 
+            contentType: 'application/json', 
+            success: function(response) {
+                console.log(response);
+                $('#editPetModal').modal('hide');
+                $('#pet-name').text(petName);
+                $('#pet-species').text(petSpecies);
+                $('#pet-breed').text(petBreed);
+                $('#pet-age').text(petAge);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+
+    $('#deletePetButton').click(function() {
+        var confirmDelete = confirm("Are you sure you want to delete this pet?");
+        if (confirmDelete) {
+            $.ajax({
+                url: `http://localhost:8000/api/pets/${petId}`,
+                method: 'DELETE',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('authToken')
+                },
+                success: function(response) {
+                    console.log(response);
+                    window.location.href = '/pets';
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        }
+    });
 });
--- front/src/js/getpost.js
+++ front/src/js/getpost.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var id = window.location.pathname.split('/').pop(); // extracts the last part of the URL path
-    var token = localStorage.getItem('authToken'); // 
+    var token = localStorage.getItem('authToken'); 
 
     $.ajax({
         url: 'http://localhost:8000/api/posts/' + id, 
@@ -16,9 +16,78 @@ $(document).ready(function() {
             $('#username').text(post.username);
             $('#petname').text(post.petname);
             $('#content').text(post.content);
+
+            var currentUsername = localStorage.getItem('username'); 
+            var postUsername = $('#username').text();
+
+            if (currentUsername === postUsername) {
+                $('#editPostButton').show();
+                $('#deletePostButton').show();
+            }
         },
         error: function(error) {
             console.log(error);
+        }
+    });
+
+    $('#editPostButton').click(function() {
+        var title = $('#title').text();
+        var content = $('#content').text();
+
+        $('#editTitle').val(title);
+        $('#editContent').val(content);
+
+        $('#editPostModal').modal('show');
+    });
+
+    $('#saveChangesButton').click(function() {
+        var title = $('#editTitle').val();
+        var content = $('#editContent').val();
+    
+        console.log(title);
+        console.log(content);
+    
+        $.ajax({
+            url: 'http://localhost:8000/api/posts/' + id, 
+            type: 'PUT',
+            contentType: 'application/json', // Specify the content type
+            headers: {
+                'Authorization': 'Bearer ' + token 
+            },
+            data: JSON.stringify({ // Stringify the data
+                title: title,
+                content: content
+            }),
+            success: function(data) {
+                console.log(data);
+                window.location.href = '/post/' + id;
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    
+        $('#editPostModal').modal('hide');
+    });
+
+    $('#deletePostButton').click(function() {
+        var confirmation = confirm("Are you sure you want to delete this post?");
+    
+        if (confirmation) {
+            $.ajax({
+                url: 'http://localhost:8000/api/posts/' + id, 
+                type: 'DELETE',
+                headers: {
+                    'Authorization': 'Bearer ' + token 
+                },
+                success: function(data) {
+                    console.log(data);
+                    window.location.href = '/'; // Redirect to the homepage or wherever you want
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
         }
     });
 });
